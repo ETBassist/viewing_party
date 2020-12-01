e145fbcf8e534682a2d46dd0cb7c245348816e40
Eduardo Parra <helloeduardo@icloud.com>
2020-12-01 16:10:31 -0700

    Add sad path registration
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -16,5 +16,17 @@ describe "User New Page/Registration" do
 
       expect(page).to have_content("Welcome, Brian!")
     end
+
+    it "I cannot create a new user with empty fields" do
+      visit '/register'
+
+      fill_in :name, with: "Brian"
+      fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "password"
+
+      click_on "Register"
+
+      expect(page).to have_content("Email can't be blank")
+    end
   end
 end
