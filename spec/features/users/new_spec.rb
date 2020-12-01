ed903ec8cbeb03a7aaa5403aa60964e5be6eba2e
Eduardo Parra <helloeduardo@icloud.com>
2020-12-01 16:24:59 -0700

    Add sad path registration.
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -22,11 +22,26 @@ describe "User New Page/Registration" do
 
       fill_in :name, with: "Brian"
       fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "pass"
+
+      click_on "Register"
+
+      expect(page).to have_content("Password confirmation doesn't match Password and Email can't be blank")
+    end
+
+    it "I cannot create a new user with pre-existing email" do
+      User.create!(email: 'user@example.com', name: 'User2', password: '123')
+
+      visit '/register'
+
+      fill_in :email, with: "user@example.com"
+      fill_in :name, with: "Brian"
+      fill_in :password, with: "password"
       fill_in :password_confirmation, with: "password"
 
       click_on "Register"
 
-      expect(page).to have_content("Email can't be blank")
+      expect(page).to have_content("Email has already been taken")
     end
   end
 end
