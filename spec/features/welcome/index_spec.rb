136fea602c8eb3c1e1647b75e561af26bf9129a5
Eduardo Parra <helloeduardo@icloud.com>
2020-12-01 14:51:40 -0700

    Add test for welcome page
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -6,6 +6,11 @@ describe "Welcome Page" do
       visit "/"
 
       expect(current_path).to eq("/")
+
+      expect(page).to have_content("Welcome to Viewing Party")
+      expect(page).to have_content("Viewing party is an application in which users can explore movie options and create a viewing party event for the user and friends.")
+      expect(page).to have_button("Login")
+      expect(page).to have_link("New to Viewing Party? Register Here")
     end
   end
 end
