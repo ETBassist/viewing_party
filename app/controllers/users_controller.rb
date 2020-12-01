f5a3d5f98a75acc860c5442c3ba8d2c14e60e098
Eduardo Parra <helloeduardo@icloud.com>
2020-12-01 16:02:38 -0700

    Add users controller
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.name}!"
+      redirect_to '/dashboard'
+    else
+      flash[:notice] = @user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.permit(:name, :email, :password)
+    end
+end
