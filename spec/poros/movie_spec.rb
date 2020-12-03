8942b68a7129525301d2593fac332a8f6f240616
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-03 14:48:24 -0700

    Add spec for movie poro
    Co-Authored-By: Eduardo Parra <helloeduardo@users.noreply.github.com>
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>

---
+++ spec/poros/movie_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe Movie do
+  it "exists with attributes", :vcr do
+
+    json_credits_response = File.read('spec/fixtures/poros/movies/fight_club_credits.json')
+    json_details_response = File.read('spec/fixtures/poros/movies/fight_club_details.json')
+    json_reviews_response = File.read('spec/fixtures/poros/movies/fight_club_reviews.json')
+
+    credits = JSON.parse(json_credits_response, symbolize_names: true)
+    details = JSON.parse(json_details_response, symbolize_names: true)
+    reviews = JSON.parse(json_reviews_response, symbolize_names: true)
+
+    movie = Movie.new(details, credits, reviews)
+
+    expect(movie.title).to eq("Fight Club")
+    expect(movie.vote_average).to eq(8.4)
+    expect(movie.runtime).to eq(139)
+    expect(movie.genres).to eq([{:id=>18, :name=>"Drama"}])
+    expect(movie.genre_names).to eq(["Drama"])
+    expect(movie.description.class).to eq(String)
+    expect(movie.cast_members.class).to eq(Array)
+  end
+end
