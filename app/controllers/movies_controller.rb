61edc21247ee1aaba535cd86b87aac624719a288
Eugene Theriault <hybridbassist@gmail.com>
2020-12-02 18:39:54 -0800

    Add search functionality and some sad path handling to movies index action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,25 +2,31 @@ class MoviesController < ApplicationController
   def index
     movie_container = []
 
-    response = conn.get('/3/movie/top_rated?page=1')
-    movie_container << get_json('/3/movie/top_rated?page=1')
-
-    response = conn.get('/3/movie/top_rated?page=2')
-    movie_container << get_json('/3/movie/top_rated?page=2')
+    if params[:search]
+      response = get_json("/3/search/movie?query=#{params[:search]}&page=2")
+      movie_container << response[:results]
+      movie_container << get_json("/3/search/movie?query=#{params[:search]}&page=2") if response[:total_pages] > 1
+    else
+      movie_container << get_json('/3/movie/top_rated?page=1')[:results]
+      movie_container << get_json('/3/movie/top_rated?page=2')[:results]
+    end
 
     @movies = movie_container.flatten
+    if @movies.empty?
+      flash[:notice] = "No movies have matched your request! Please try again!"
+    end
   end
 
   private
 
   def conn
-    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.params['api_key'] = ENV['MOVIE_DB_API_KEY']
     end
   end
 
   def get_json(url)
     response = conn.get(url)
-    JSON.parse(response.body, symbolize_names: true)[:results]
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
