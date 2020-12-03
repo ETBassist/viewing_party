50b75b1b0644049c873c54c9c66cbb9c066d5df1
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-02 17:42:39 -0700

    Refactor controller, changes nothing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,26 @@
 class MoviesController < ApplicationController
   def index
     movie_container = []
-    conn = Faraday.new('https://api.themoviedb.org') do |faraday|
+
+    response = conn.get('/3/movie/top_rated?page=1')
+    movie_container << get_json('/3/movie/top_rated?page=1')
+
+    response = conn.get('/3/movie/top_rated?page=2')
+    movie_container << get_json('/3/movie/top_rated?page=2')
+
+    @movies = movie_container.flatten
+  end
+
+  private
+
+  def conn
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.params['api_key'] = ENV['MOVIE_DB_API_KEY']
     end
-    response = conn.get('/3/movie/top_rated?page=1')
-    movie_container << JSON.parse(response.body, symbolize_names: true)[:results]
-    response = conn.get('/3/movie/top_rated?page=2')
-    movie_container << JSON.parse(response.body, symbolize_names: true)[:results]
-    @movies = movie_container.flatten
+  end
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
