cf5b7eafbe9e04038fd2400e5ac6b884f626566b
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-03 15:41:41 -0700

    Added reviews count method
    Co-Authored-By: Eduardo Parra <helloeduardo@users.noreply.github.com>
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -5,24 +5,28 @@ class Movie
               :description,
               :genres,
               :cast_members,
-              :reviews
+              :reviews,
+              :reviews_count
 
   def initialize(movie_details, movie_credits, movie_reviews)
     @title = movie_details[:original_title]
     @vote_average = movie_details[:vote_average]
     @runtime = movie_details[:runtime]
-    @description =  movie_details[:overview]
+    @description = movie_details[:overview]
     @genres = movie_details[:genres]
     @cast_members = movie_credits[:cast]
     @reviews = movie_reviews[:results]
-  end
-
-  def reviews_count
-    @reviews[:total_results]
+    @reviews_count = movie_reviews[:total_results]
   end
 
   def top_ten_cast_members
-    @cast_members[0..9]
+    @cast_members[0..9].map do |member|
+      CastMember.new(member)
+    end
+  end
+
+  def formatted_runtime
+    "#{@runtime/60}h #{@runtime % 60}min"
   end
 
   def genre_names
