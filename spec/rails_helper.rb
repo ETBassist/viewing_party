f5445b697b1a9c58f11657fa4d824e691b4a56b7
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-03 13:31:15 -0700

    Add vcr and webmock to gems, add vcr to movies index spec
    Co-Authored-By: Eduardo Parra <helloeduardo@users.noreply.github.com>
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'webmock/rspec'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
@@ -69,3 +70,11 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data('<DONT_EXPOSE_MY_DATA_PLEASE>') { ENV['MOVIE_DB_API_KEY'] }
+  config.default_cassette_options = { re_record_interval: 30.days }
+end 
