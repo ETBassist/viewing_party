3947f097b45fa501c39c26a8dac94a1c2206ea66
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-03 15:39:41 -0700

    Add movie poro to show method
    Co-Authored-By: Eduardo Parra <helloeduardo@users.noreply.github.com>
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,9 +15,11 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie_details = get_json("/3/movie/#{params[:id]}")
-    @movie_credits = get_json("/3/movie/#{params[:id]}/credits")
-    @movie_reviews = get_json("/3/movie/#{params[:id]}/reviews")
+    movie_details = get_json("/3/movie/#{params[:id]}")
+    movie_credits = get_json("/3/movie/#{params[:id]}/credits")
+    movie_reviews = get_json("/3/movie/#{params[:id]}/reviews")
+
+    @movie = Movie.new(movie_details, movie_credits, movie_reviews)
   end
 
   private
