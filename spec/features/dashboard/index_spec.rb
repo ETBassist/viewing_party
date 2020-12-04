ff3b653dd307b7a3e6a41d0e115a23550f4b30fd
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-03 17:44:51 -0700

    Test dashboard friends
    Co-Authored-By: Eduardo Parra <helloeduardo@users.noreply.github.com>
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -4,6 +4,7 @@ describe 'Dashboard' do
   describe 'As a user' do
     before :each do
       @user = User.create(name: 'Brian', email: 'user@example.com', password: 'password')
+      @user_2 = User.create(name: 'Bob Vance', email: 'user2@example.com', password: 'password')
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
@@ -23,6 +24,36 @@ describe 'Dashboard' do
       click_button('Discover Movies')
 
       expect(current_path).to eq('/discover')
+    end
+
+    it "I can add and see my friends" do
+      visit '/dashboard'
+
+      within(".friends") do
+        expect(page).to have_button("Add Friend")
+        expect(page).to have_field(:friend_email)
+
+        fill_in :friend_email, with: @user_2.email
+        click_button "Add Friend"
+      end
+
+      expect(current_path).to eq("/dashboard")
+      expect(page).to have_content("You have added #{@user_2.name} as a friend")
+
+      within(".current_friends") do
+        expect(page).to have_content(@user_2.name)
+      end
+    end
+
+    it "I cant add a friend with an invalid email" do
+      visit '/dashboard'
+
+      within(".friends") do
+        fill_in :friend_email, with: "garbage@example.com"
+        click_button "Add Friend"
+      end
+
+      expect(page).to have_content("I'm sorry, your friend cannot be found :(")
     end
   end
 end
