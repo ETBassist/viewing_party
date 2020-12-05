e7fa038a5b5d3cb54de5a35f550fd4a14a7f3315
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-05 12:02:36 -0700

    Add viewing party resources for new and create

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   get '/movies/:id', to: 'movies#show'
 
   post '/friendships', to: 'friendships#create'
+
+  resources :viewing_party, only: [:new, :create]
 end
