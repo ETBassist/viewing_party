6e598b0bb993d279dd5fabb3908e974b1f6cc69a
Eugene Theriault <hybridbassist@gmail.com>
2020-12-05 14:50:01 -0800

    Create has_many relationship between users and parties

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,3 +1,6 @@
 class Party < ApplicationRecord
   validates_presence_of :date, :duration, :start_time, :movie_title
+
+  has_many :invitations
+  has_many :users, through: :invitations
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,7 @@ describe User do
   describe "Relationship" do
     it {should have_many(:friendships)}
     it {should have_many(:friends).through :friendships}
+    it { should have_many(:invitations) }
+    it { should have_many(:parties).through(:invitations) }
   end
 end
