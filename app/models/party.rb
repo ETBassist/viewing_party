9a6dd666a7e4e09ebc60bab8ce94232f24e87d8b
Eugene Theriault <hybridbassist@gmail.com>
2020-12-05 15:17:43 -0800

    Move host status to party table from invitations

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,5 @@
 class Party < ApplicationRecord
-  validates_presence_of :date, :party_duration, :time, :movie_title
+  validates_presence_of :date, :party_duration, :time, :movie_title, :host_id
 
   has_many :invitations
   has_many :users, through: :invitations
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_225525) do
+ActiveRecord::Schema.define(version: 2020_12_05_231148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2020_12_05_225525) do
   create_table "invitations", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "party_id"
-    t.boolean "host"
     t.index ["party_id"], name: "index_invitations_on_party_id"
     t.index ["user_id"], name: "index_invitations_on_user_id"
   end
@@ -37,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_12_05_225525) do
     t.string "date"
     t.string "time"
     t.string "movie_title"
+    t.integer "host_id"
   end
 
   create_table "users", force: :cascade do |t|
