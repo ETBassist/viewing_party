76348b0d633af3746c5d36e5773a5e4b14834827
Eduardo Parra <helloeduardo@icloud.com>
2020-12-07 18:39:47 -0700

    Add movies facade methods
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -4,14 +4,6 @@ class MoviesFacade
     Movie.new(movie_json)
   end
 
-
-
-
-
-  def self.movie_service
-    MovieService.new(data)
-  end
-
   def self.top_rated_movies
     MovieService.top_rated_movies.map do |movie_data|
       Movie.new(movie_data)
@@ -19,6 +11,8 @@ class MoviesFacade
   end
 
   def self.movies_by_keyword(name)
-
+    MovieService.movies_by_keyword(name).map do |movie_data|
+      Movie.new(movie_data)
+    end
   end
 end
