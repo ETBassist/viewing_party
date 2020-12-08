de8611b3b34d47b491fca8d9322273452a595f25
Eduardo Parra <helloeduardo@icloud.com>
2020-12-07 19:04:50 -0700

    refactor for new fixture
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>

--- spec/poros/cast_member_spec.rb
+++ spec/poros/cast_member_spec.rb
@@ -3,10 +3,10 @@ require "rails_helper"
 RSpec.describe CastMember do
   it "exists with attributes" do
 
-    json_credits_response = File.read('spec/fixtures/poros/movies/fight_club_credits.json')
-    credits = JSON.parse(json_credits_response, symbolize_names: true)
+    json_credits_response = File.read('spec/fixtures/poros/movies/fight_club_details.json')
+    response = JSON.parse(json_credits_response, symbolize_names: true)
 
-    cast_member = CastMember.new(credits[:cast][0])
+    cast_member = CastMember.new(response[:credits][:cast][0])
 
     expect(cast_member.class).to eq(CastMember)
     expect(cast_member.actor).to eq("Edward Norton")
