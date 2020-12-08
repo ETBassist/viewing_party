098047d9c1fef67875cd3d9189665beb0d0ab9ef
Eduardo Parra <helloeduardo@icloud.com>
2020-12-07 19:06:02 -0700

    Move methods to private
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,14 +1,4 @@
 class MovieService
-  def self.conn
-    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
-      faraday.params['api_key'] = ENV['MOVIE_DB_API_KEY']
-    end
-  end
-
-  def self.get_json(url)
-    response = self.conn.get(url)
-    JSON.parse(response.body, symbolize_names: true)
-  end
 
   def self.top_rated_movies
     movies = get_json('/3/movie/top_rated?page=1')[:results]
@@ -25,5 +15,17 @@ class MovieService
 
   def self.movie(id)
     get_json("/3/movie/#{id}?append_to_response=credits,reviews")
+  end
+
+  private
+  def self.conn
+    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.params['api_key'] = ENV['MOVIE_DB_API_KEY']
+    end
+  end
+
+  def self.get_json(url)
+    response = self.conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
